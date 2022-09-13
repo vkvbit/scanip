20dbe2e53c8199a9d584d4fe95dd68dd42048a48
Vaibhav Kumar <60957669+vkvbit@users.noreply.github.com>
2022-09-13 13:22:48 +0530

    Main added nikto

--- scanip.sh
+++ scanip.sh
@@ -3,4 +3,5 @@ target_ip='IP address of target'
 xterm -hold -e zsh -c "ping -n 3 ${target_ip}" & 
 xterm -hold -e zsh -c "nmap -oX ./nmap_result.xml -sC -sV ${target_ip}" & 
 xterm -hold -e zsh -c "whois ${target_ip}" & 
-xterm -hold -e zsh -c "nslookup ${target_ip}"
+xterm -hold -e zsh -c "nslookup ${target_ip}" & 
+xterm -hold -e zsh -c "nikto -h ${target_ip}"
