f3ca84d1b625e1af04c20dc53ca2ad47c789447e
imvkv <91961569+imvkv@users.noreply.github.com>
2022-10-01 20:18:03 +0530

    Update scanip.sh

--- scanip.sh
+++ scanip.sh
@@ -4,4 +4,5 @@ xterm -hold -e zsh -c "ping -n 3 ${target_ip}" &
 xterm -hold -e zsh -c "nmap -oX ./nmap_result.xml -sC -sV ${target_ip}" & 
 xterm -hold -e zsh -c "whois ${target_ip}" & 
 xterm -hold -e zsh -c "nslookup ${target_ip}" & 
-xterm -hold -e zsh -c "nikto -h ${target_ip}"
+xterm -hold -e zsh -c "nikto -h ${target_ip}" & 
+xterm -hold -e zsh -c "python3 AORT.py -d ${target_ip}"
